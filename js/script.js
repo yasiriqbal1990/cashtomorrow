43db6d5edd659eb1628e028f8c0e0373c0f4e7bd
yasir <yasiriqbal1990@gmail.com>
2022-10-14 23:09:58 +0500

    second commit

---
+++ js/script.js
@@ -0,0 +1,68 @@
+// Chnage background image
+
+//bg.style.backgroundImage = "url('/images/hero-img.jpg')";
+
+function chageBackgroundImageRandomly() {
+  let bg = document.querySelector(".hero-section");
+
+  // 3 image
+  let allImage = [
+    "/images/hero-img.jpg",
+    "/images/bg-2.jpg",
+    "/images/bg-3.jpg",
+  ];
+  let randomIndex = Math.floor(Math.random() * allImage.length);
+  // 0, 1 or 2
+
+  bg.style.backgroundImage = "url('" + allImage[randomIndex] + "')";
+}
+chageBackgroundImageRandomly();
+// lazy load Images
+document.addEventListener("DOMContentLoaded", function () {
+  var lazyloadImages;
+
+  if ("IntersectionObserver" in window) {
+    lazyloadImages = document.querySelectorAll(".lazy");
+    var imageObserver = new IntersectionObserver(function (entries, observer) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          var image = entry.target;
+          image.classList.remove("lazy");
+          imageObserver.unobserve(image);
+        }
+      });
+    });
+
+    lazyloadImages.forEach(function (image) {
+      imageObserver.observe(image);
+    });
+  } else {
+    var lazyloadThrottleTimeout;
+    lazyloadImages = document.querySelectorAll(".lazy");
+
+    function lazyload() {
+      if (lazyloadThrottleTimeout) {
+        clearTimeout(lazyloadThrottleTimeout);
+      }
+
+      lazyloadThrottleTimeout = setTimeout(function () {
+        var scrollTop = window.pageYOffset;
+        lazyloadImages.forEach(function (img) {
+          if (img.offsetTop < window.innerHeight + scrollTop) {
+            img.src = img.dataset.src;
+            img.classList.remove("lazy");
+          }
+        });
+        if (lazyloadImages.length == 0) {
+          document.removeEventListener("scroll", lazyload);
+          window.removeEventListener("resize", lazyload);
+          window.removeEventListener("orientationChange", lazyload);
+        }
+      }, 20);
+    }
+
+    document.addEventListener("scroll", lazyload);
+    window.addEventListener("resize", lazyload);
+    window.addEventListener("orientationChange", lazyload);
+  }
+});
